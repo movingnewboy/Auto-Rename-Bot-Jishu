6ec896cdc165964320deaf3530a495947f7c1a35
movingnewboy <moovingnewboy@gmail.com>
2025-01-31 00:14:54 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -5,6 +5,7 @@ from config import Config, Txt
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
+    start_time = float(start) if isinstance(start, str) else start
     now = time.time()
     diff = now - start
     if round(diff % 5.00) == 0 or current == total:        
