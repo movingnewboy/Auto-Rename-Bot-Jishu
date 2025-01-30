44097a6bb489bc4d9240bdb356d7d4a949e80a9a
movingnewboy <moovingnewboy@gmail.com>
2025-01-30 21:01:04 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -136,7 +136,7 @@ episode_number = extract_episode_number(filename)
 print(f"Extracted Episode Number: {episode_number}")
 
 # Inside the handler for file uploads
-@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
+# @Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
 async def auto_rename_files(client, message):
     user_id = message.from_user.id
     firstname = message.from_user.first_name
